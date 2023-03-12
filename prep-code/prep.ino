4d390c3786e4b975c3f71912fb77f5a6b4d1c739
Tony Fischetti <tony.fischetti@gmail.com>
2023-03-12 17:27:40 -0400

    getting somewhere with this lib. But let's try another

--- prep-code/prep.ino
+++ prep-code/prep.ino
@@ -5,22 +5,30 @@
 //            of the new firmware?
 
 
+#include <elapsedMillis.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
+#include <TEA5767N.h>
 
-/////
-#include "Waveshare_LCD1602_RGB.h"
 
-/* #define LCE_ADDRESS (0x7c >> 1) */
+#define DT_UPDATE_EVERY 500
+#define RE_CHECK_EVERY  30
 
-/* Waveshare_LCD1602_RGB lce(16,2);  //16 characters and 2 lines of show */
-/////
+#define I2C_COLLECTOR 3
+#define RE_DT_LAG     8
+#define RE_CLK        9 // blue
+
+#define DEBUG true
+
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 RTC_DS3231 rtc;
 
+TEA5767N radio = TEA5767N();
 
+elapsedMillis dt_timer;
+elapsedMillis re_timer;
 
 DateTime now;
 char the_date[11];
@@ -29,60 +37,11 @@ char the_temp[5];
 
 char tmp_date[11];
 
-
 unsigned long counter = 0;
 
-
-/* void disp_the_date() { */
-/*     strcpy(tmp_date, the_date); */
-/*     byte data[3] = {0x40, 0}; */
-/*     for (byte i = 0; i < 10; i++) { */
-/*         data[2] = tmp_date[i]; */
-/*         lce.write_char(tmp_date[i]); */
-/*     } */
-/* } */
-/*  */
-/* void disp_the_time() { */
-/*     for (byte i = 0; i < 8; i++) { */
-/*         lce.write_char(the_time[i]); */
-/*         delay(100); */
-/*     } */
-/* } */
+float current_station = 104.3;
 
 
-void setup() {
-
-    Serial.begin(115200);
-
-    if (!rtc.begin()) {
-        Serial.println("Couldn't find RTC");
-        Serial.flush();
-        while (1) delay(10);
-    }
-
-    /*
-    if (rtc.lostPower()) {
-        Serial.println("RTC lost power, let's set the time!");
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    }
-    */
-
-    lcd.init();
-    lcd.backlight();
-
-    /* lce.init(); */
-    /* lce.noAutoscroll(); */
-
-    /* lce.setCursor(0,0); */
-    /* lce.send_string(the_date); */
-    /* lce.write_char('0'); */
-    /* lce.write_char('1'); */
-    /* lce.write_char('2'); */
-    /* lce.setCursor(0,1); */
-    /* lce.send_string("0123456789a"); */
-
-
-}
 
 
 void set_now() {
@@ -114,58 +73,160 @@ void set_temp() {
 }
 
 
-void loop() {
 
-    set_now();
-    set_date();
-    set_time();
-    set_temp();
+void turn_radio_i2c_on() {
+    digitalWrite(I2C_COLLECTOR, HIGH);
+}
 
-    /* Serial.println(now); */
-    /* Serial.println(the_date); */
-    /* Serial.println(the_time); */
-    /* Serial.println(); */
+void turn_radio_i2c_off() {
+    digitalWrite(I2C_COLLECTOR, LOW);
+}
+
+
+
+void update_rotary_encoder() {
+    bool current_state_CLK = digitalRead(RE_CLK);
+    /* Serial.println(current_state_CLK); */
+    static bool previous_state_CLK = current_state_CLK;
+    bool current_state_DT;
+
+    current_state_CLK = digitalRead(RE_CLK);
+    if (current_state_CLK != previous_state_CLK  && current_state_CLK == 1){
+
+        turn_radio_i2c_on();
+        current_state_DT = digitalRead(RE_DT_LAG);
+        // increment
+        if (current_state_DT != current_state_CLK) {
+            #if DEBUG
+            Serial.println(F("supposed to increment"));
+            radio.setSearchUp();
+            radio.searchNext();
+            #endif
+        }
+        // decrement
+        else {
+            #if DEBUG
+            Serial.println(F("supposed to decrement"));
+            #endif
+            radio.setSearchDown();
+            radio.searchNext();
+        }
+        current_station = radio.readFrequencyInMHz();
+        Serial.print("_now_ it's: ");
+        Serial.println(current_station);
+        Serial.print("strength: ");
+        Serial.println(radio.getSignalLevel());
+        Serial.println();
+
+        turn_radio_i2c_off();
+    }
+    previous_state_CLK = current_state_CLK;
+}
+
+
+
+void setup() {
+
+    pinMode(I2C_COLLECTOR, OUTPUT);
+    pinMode(RE_CLK,        INPUT);
+    pinMode(RE_DT_LAG,     INPUT);
+    /* pinMode(RE_SW_BUTTON,  INPUT_PULLUP); */
+
+    Serial.begin(115200);
+
+    if (!rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        Serial.flush();
+        while (1) delay(10);
+    }
 
     /*
-    0123456789abcdef
-      0123-55-89
-    12:45
+    if (rtc.lostPower()) {
+        Serial.println("RTC lost power, let's set the time!");
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
     */
 
-    // TODO TODO: sometimes you'll have to clear it
-    //            12:59 -> 1:00
-    //            10 C -> 9 C
-    // TODO TODO: put a degree character in!
+    lcd.init();
+    lcd.backlight();
 
-    /* lcd.clear(); */
-    lcd.setCursor(3, 0);
-    lcd.print(the_date);
-    lcd.setCursor(1, 1);
-    lcd.print(the_time);
-    lcd.setCursor(11, 1);
-    lcd.print(the_temp);
+    /* radio.setMonoReception(); */
+    /*  */
+    /* radio.setStereoNoiseCancellingOn(); */
 
-    if (counter % 10 == 0) {
-        Serial.print(the_date);
-        Serial.print(" - ");
-        Serial.println(the_time);
-        Serial.println();
+    // TRANSISTORS!!
+    turn_radio_i2c_on();
+    radio.selectFrequencyMuting(current_station);
+    Serial.println(radio.getSignalLevel());
+    turn_radio_i2c_off();
+
+}
+
+
+
+
+void loop() {
+
+    if (re_timer > RE_CHECK_EVERY) {
+        update_rotary_encoder();
+        re_timer = 0;
     }
 
 
-    /* if (counter % 10 == 0) { */
-    /*     lce.clear(); */
-    /*     lce.setCursor(0, 0); */
-    /*     lce.write_char('a'); */
-    /*     disp_the_time(); */
-    /* } */
+    if (dt_timer > DT_UPDATE_EVERY) {
 
-    /* Serial.println(); */
+        /* Serial.println("triggered"); */
+
+        set_now();
+        set_date();
+        set_time();
+        set_temp();
+
+        // TODO TODO: sometimes you'll have to clear it
+        //            12:59 -> 1:00
+        //            10 C -> 9 C
+        // TODO TODO: put a degree character in!
+
+        /* lcd.clear(); */
+
+        lcd.setCursor(3, 0);
+        lcd.print(the_date);
+        lcd.setCursor(1, 1);
+        lcd.print(the_time);
+        lcd.setCursor(11, 1);
+        lcd.print(the_temp);
+
+        dt_timer = 0;
+    }
 
 
-    delay(500);
-    /* delay(500); */
-    /* delay(500); */
+    
+    if (counter % 500001 == 0) {
+
+        // Serial.print(the_date);
+        // Serial.print(" - ");
+        // Serial.println(the_time);
+        // Serial.println();
+        
+        /* Serial.println("about to change stations"); */
+
+        /* radio.startsSearchFrom(current_station); */
+        /* delay(10); */
+        /* current_station = radio.readFrequencyInMHz(); */
+        /* Serial.print("now: "); */
+        /* Serial.println(current_station); */
+
+        // turn_radio_i2c_on();
+        // if (counter % 2 == 0)
+            // radio.selectFrequencyMuting(97.1);
+        // else
+            // radio.selectFrequencyMuting(104.3);
+        // turn_radio_i2c_off();
+
+        Serial.println("");
+
+    }
+
 
     ++counter;
 
